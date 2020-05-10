46e4281798b81ecc133678ea0f02e98a8dec113a
yangjie <jie.yang@witontek.com>
2020-05-10 16:19:47 +0800

    update--

--- mi/src/service/article/article.service.ts
+++ mi/src/service/article/article.service.ts
@@ -1,12 +1,26 @@
 import { Injectable } from '@nestjs/common';
 import { ArticleModel } from 'src/model/article.model';
+import { ArticleDto } from 'src/dto/article.dto';
+import { InjectModel } from 'nestjs-typegoose';
 
 @Injectable()
 export class ArticleService {
 
+    // 将ArticleModel以依赖注入的方式引入进来
+    constructor(
+        @InjectModel(ArticleModel) private readonly articleModel: any,
+    ) {
+
+    }
+
     // 查询全部文章
-    async findAll() {
-        return await ArticleModel.find();
+    async getAllArticles() {
+        return await this.articleModel.find();
+    }
+
+    // 创建一篇文章
+    async addArticle(articleDto: ArticleDto) {
+        return await this.articleModel.create(articleDto);
     }
 
 }
